acf24b0f7042dcc1e439cdc327940796e05781a7
Ken Reed <kenakioreed@gmail.com>
2019-10-21 18:50:01 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,17 @@ impl GameState{
 }
 */
 
+pub static color_table:&'static [[f32;3]]=
+    &[
+        [0.0,1.0,0.0],
+        [0.9,0.9,0.9],
+        [1.0,0.2,0.2],
+        [0.6,0.5,1.0],
+        [1.0,1.0,0.0],
+        [1.0,0.0,1.0],
+        [0.0,1.0,1.0],
+    ];
+
 pub struct Symbols{
     digit_table:ascii_num::digit::DigitSymbolTable,
     game_table:ascii_num::GameSymbolTable,
@@ -113,8 +124,7 @@ fn main() {
 
 
 
-    let mut bot_verticies=Mutex::new(Vec::new());
-
+    
     let mut bot_buffer=glsys.create_vbo(0);
     //glsys.set_bot_color(game_response.color.unwrap());
     
@@ -129,203 +139,150 @@ fn main() {
     let mut mousepos=vec2(0.0,0.0);
     #[derive(Clone,Debug)]
     struct GameInputs{
+        shut_down:bool,
         keystrokes:Vec<VirtualKeyCode>,
         mouseposes:Vec<Vec2<f32>>
     }
 
-    let mut game_inputs=GameInputs{keystrokes:Vec::new(),mouseposes:Vec::new()};
-    let game_inputs=Mutex::new(game_inputs);
+    let mut color_table_counter=0;
 
-
+    let mut game_inputs=GameInputs{shut_down:false,keystrokes:Vec::new(),mouseposes:Vec::new()};
     
-    let (tx, rx): (mpsc::Sender<GameResponse>, mpsc::Receiver<GameResponse>) = mpsc::channel();
 
     //TODO talk to glutin about why there is a static lifetime bound.
-    let game_inputs_ref:&Mutex<GameInputs>=unsafe{&*(&game_inputs as *const _)};
-    let bot_verticies_ref:&Mutex<Vec<Vertex>>=unsafe{&*(&bot_verticies as *const _)};
+    let mut last_time:Option<std::time::Instant>=None;
+    
+    events_loop.run(move |event,_,control_flow| {
+        match event {
 
-    crossbeam::thread::scope(move |s| {
-        s.spawn(move |s| {
-            let mut border=border;
-            loop{
-                let kk=std::time::Instant::now();
-
-
-                println!("game loop");
-                let game_inputs = {
-                    let mut gg=game_inputs_ref.lock().unwrap();
-                    let game_inputs=gg.clone();
-                    gg.keystrokes.clear();
-                    gg.mouseposes.clear();
-                    game_inputs
-                };
-                //dbg!(game_inputs.border);
-
-                //mutex get mouse pos    
-                let game_response=display1.step(&game_inputs.mouseposes,&border,&symbols,&game_inputs.keystrokes);
-
-                
-                let game_response = if game_response.next_world{
-                    let k=Display2::new(&symbols);
-                    display1=Box::new(k.0);
-                    k.1
-                }else{
-                    game_response
-                };
-
-                if let Some(new_game_world)= game_response.new_game_world{
-                    border=compute_border(new_game_world.0,[startx as f32,starty as f32]);
-                    for _ in 0..100{
-                        display1.step(&game_inputs.mouseposes,&border,&symbols,&game_inputs.keystrokes);
-                    }
-                }
-
-                tx.send(game_response);
-                
-
-                {
-                    let mut bot_verticies=bot_verticies_ref.lock().unwrap();
-                    bot_verticies.clear();
-                    for b in display1.get_bots(){
-                        bot_verticies.push(Vertex([b.pos.x,b.pos.y,1.0]))    
-                    }
-                }
-                
-                let elapsed=kk.elapsed().as_millis() as u64;
-                println!("elapsed={:?}",elapsed);
-                //notify main thread to draw
-
-                let elapsed = if elapsed>16 {16}else{elapsed};
-                std::thread::sleep(std::time::Duration::from_millis(16-elapsed));
-                
-            }
-        });
-
-
-
-        events_loop.run(move |event,_,control_flow| {
-            //*c=glutin::event_loop::ControlFlow::Exit;
-            
-            //dbg!(&event);
-            match event {
-
-                Event::WindowEvent{ event, .. } => match event {
-                    WindowEvent::KeyboardInput{input,..}=>{
-                        match input.virtual_keycode{
-                            Some(VirtualKeyCode::Escape)=>{println!("close requested");*control_flow=ControlFlow::Exit},
-                            _=>{}
-                        }
-
-                        if input.state==ElementState::Released{
-                            let mut game_inputs=game_inputs_ref.lock().unwrap();
-                            if let Some(k)=input.virtual_keycode{
-                                game_inputs.keystrokes.push(k);
-                            }
-
-                            if let Some(k)=input.virtual_keycode{
-                                if k==VirtualKeyCode::C{
-                                    game_inputs.mouseposes.push(mousepos);
-                                    //mouse_active=true
-                                }
-                            }
-                        }
-
-
-                    },
-                    WindowEvent::CloseRequested => {println!("close requested!");running=false;},
-                    WindowEvent::Resized(logical_size) => {
-                        println!("reisezed={:?}",logical_size);
-                        //glsys.set_camera_and_bot_radius(border,radius);
-                    },
-                    
-                    WindowEvent::CursorMoved{modifiers:_,device_id:_,position:logical_position} => {
-                        let glutin::dpi::LogicalPosition{x,y}=logical_position;
-                        let mousepos2=vec2(x as f32,y as f32);
-                        //let mut va:Vec<Vec2<f32>>=poses.iter().map(|a|a.pos).collect();
-                        
-                        let mouseposx=mousepos2.x-(startx as f32/2.0);
-                        let mouseposy=mousepos2.y-(starty as f32/2.0);
-                    
-                        let ((x1,x2),(y1,y2))=border.get();
-                        let w=x2-x1;
-                        let h=y2-y1;
-
-                        let mouseposx=mouseposx*(w/startx as f32);
-                        let mouseposy=mouseposy*(h/starty as f32);
-                       
-                        mousepos=vec2(mouseposx,mouseposy);
-                    },
-                    WindowEvent::RedrawRequested => {
-                        println!("redraaaaw");
-                        //glsys.set_camera_and_bot_radius(border,radius);
-                           
-                        /*
-                        gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
-                        windowed_context.swap_buffers().unwrap();
-                        */
-                    }
-                    _=>{}
-                },
-                EventsCleared=>{
-                    //println!("mmmmm");
-                    //if game_response in rx.try_iter(){
-                    if let Ok(game_response)=rx.try_recv(){
-                        println!("main loop");
-                        if let Some(new_game_world)=game_response.new_game_world{
-                            println!("AYYYYY");
-                            let ((rect,radius))=new_game_world;
-                            border=compute_border(rect,[startx as f32,starty as f32]);
-                            /*
-                            {
-                            let mut vv=game_inputs_ref.lock().unwrap();
-                            vv.border=border;
-                            }
-                            */
-                            glsys.set_camera_and_bot_radius(border,radius);
+            Event::WindowEvent{ event, .. } => match event {
+                WindowEvent::KeyboardInput{input,..}=>{
                             
+                    match input.virtual_keycode{
+                        Some(VirtualKeyCode::Escape)=>{
+                            *control_flow=ControlFlow::Exit;
+                            game_inputs.shut_down=true;
+                        },
+                        _=>{}
+                    }
+
+                    if input.state==ElementState::Released{
+                        if let Some(k)=input.virtual_keycode{
+                            game_inputs.keystrokes.push(k);
                         }
 
-
-                        let ll={
-                            let bot_verticies=bot_verticies_ref.lock().unwrap();
-
-                            if bot_verticies.len()!=bot_buffer.get_num_verticies(){
-                                glsys.re_generate_buffer(&mut bot_buffer,bot_verticies.len()); 
+                        if let Some(k)=input.virtual_keycode{
+                            if k==VirtualKeyCode::C{
+                                game_inputs.mouseposes.push(mousepos);
+                                //mouse_active=true
                             }
-
-
-                            let mut counter=0;
-                            bot_buffer.update(&bot_verticies,|a|{
-                                *a
-                            });
-                            bot_verticies.len()
-                        };
-
-                        let mut ss=glsys.new_draw_session([0.0,0.0,0.0]);
-                        ss.draw_vbo_section(&bot_buffer,0,ll,color);
-                        //ss.draw_vbo_section(&bot_buffer,0,200,[1.0,0.0,3.0]);
-                        //ss.draw_vbo_section(&bot_buffer,200,display1.get_bots().len(),color);
-                        ss.finish();
-
-                        //let mut game_inputs=game_inputs.lock().unwrap();
-                        //game_inputs.mouseposes.clear();
-                        //game_inputs.keystrokes.clear();
+                        }
                     }
-                    
+
+
+                },
+                WindowEvent::CloseRequested => {println!("close requested!");running=false;},
+                WindowEvent::Resized(logical_size) => {
+                    //glsys.set_camera_and_bot_radius(border,radius);
+                },
+                WindowEvent::MouseInput{device_id:_,state:ElementState::Released,button:_,modifiers:_}=>{
+                    color_table_counter=(color_table_counter+1) % color_table.len();
                 }
-                _ => {},
-            }  
+                WindowEvent::CursorMoved{modifiers:_,device_id:_,position:logical_position} => {
+                    let glutin::dpi::LogicalPosition{x,y}=logical_position;
+                    let mousepos2=vec2(x as f32,y as f32);
+                    //let mut va:Vec<Vec2<f32>>=poses.iter().map(|a|a.pos).collect();
+                    
+                    let mouseposx=mousepos2.x-(startx as f32/2.0);
+                    let mouseposy=mousepos2.y-(starty as f32/2.0);
+                
+                    let ((x1,x2),(y1,y2))=border.get();
+                    let w=x2-x1;
+                    let h=y2-y1;
+
+                    let mouseposx=mouseposx*(w/startx as f32);
+                    let mouseposy=mouseposy*(h/starty as f32);
+                   
+                    mousepos=vec2(mouseposx,mouseposy);
+                },
+                WindowEvent::RedrawRequested => {
+                    //glsys.set_camera_and_bot_radius(border,radius);
+                       
+                    /*
+                    gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
+                    windowed_context.swap_buffers().unwrap();
+                    */
+                }
+                _=>{}
+            },
+            EventsCleared=>{
+                let do_run = match last_time{
+                    Some(last_time)=>{
+                        if last_time.elapsed().as_millis()>=16{
+                            true
+                        }else{
+                            false
+                        }
+                    },
+                    None=>{
+                        true
+                    }
+                };
+
+                if do_run{
+                    //mutex get mouse pos    
+                    let game_response=display1.step(&game_inputs.mouseposes,&border,&symbols,&game_inputs.keystrokes);
+
+                    
+                    let game_response = if game_response.next_world{
+                        let k=Display2::new(&symbols);
+                        display1=Box::new(k.0);
+                        k.1
+                    }else{
+                        game_response
+                    };
+
+                    if let Some(new_game_world)= game_response.new_game_world{
+                        border=compute_border(new_game_world.0,[startx as f32,starty as f32]);
+                        for _ in 0..100{
+                            display1.step(&game_inputs.mouseposes,&border,&symbols,&game_inputs.keystrokes);
+                        }
+                    }
+
+                    
+                    if let Some(new_game_world)=game_response.new_game_world{
+                        let ((rect,radius))=new_game_world;
+                        border=compute_border(rect,[startx as f32,starty as f32]);                            
+                        glsys.set_camera_and_bot_radius(border,radius);
+                    }
 
 
 
-        });
+                    if display1.get_bots().len()!=bot_buffer.get_num_verticies(){
+                        glsys.re_generate_buffer(&mut bot_buffer,display1.get_bots().len()); 
+                    }
 
 
-    }); 
-    
+                    let mut counter=0;
+                    bot_buffer.update(&display1.get_bots(),|b|{
+                        Vertex([b.pos.x,b.pos.y,1.0])
+                    });
 
-
-    
-    
-    
+                    let mut ss=glsys.new_draw_session([0.0,0.0,0.0]);
+                    let color=color_table[color_table_counter];
+                    ss.draw_vbo_section(&bot_buffer,0,display1.get_bots().len(),color);
+                    //ss.draw_vbo_section(&bot_buffer,0,200,[1.0,0.0,3.0]);
+                    //ss.draw_vbo_section(&bot_buffer,200,display1.get_bots().len(),color);
+                    ss.finish();
+        
+                    game_inputs.mouseposes.clear();
+                    game_inputs.keystrokes.clear();
+                    
+                    last_time=Some(std::time::Instant::now());
+                
+                }         
+            }
+            _ => {},
+        }  
+    });
 }
